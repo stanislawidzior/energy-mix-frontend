616a22d9ef2f7dd33a9285dd61af245a19fb5df0
stanidzior <stanosik123@gmail.com>
2026-01-23 18:51:10 +0100

    dalej

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import './App.css'
 
+import './App.css'
+import { useFetchEnergyMix } from './hook/useFetchEnergyMix';
+import { Chart } from 'react-chartjs-2';
+import { Chart as ChartJS, LineController, LineElement, PointElement, LinearScale, Title } from 'chart.js';
+ 
 function App() {
-  async function fetchData() {
-    const response = await fetch('http://localhost:8080/api/v1/energy-mix');
-    const data = await response.json();
-    console.log(data);
+
+  const {data,error, isFetching, isLoading} = useFetchEnergyMix();
+  if(isLoading || isFetching) {
+    return <div>Loading...</div>
   }
-  fetchData();
+  if(error) {
+    return <div>Error occurred: {(error as Error).message}</div>
+  }
+ ChartJS.register(LineController, LineElement, PointElement, LinearScale, Title);
+
   return (
     <>
-      <canvas id="myChart"></canvas>
+    <Chart type='line' data={chartData} />
     </>
   )
 }
